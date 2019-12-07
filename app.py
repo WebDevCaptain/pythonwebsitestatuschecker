2f44b630f9c14130ba52f466c58ec0b74fcabb69
Shreyash <webdevcaptain@gmail.com>
2019-12-07 19:51:33 +0530

    v1.0.1

--- app.py
+++ app.py
@@ -4,26 +4,37 @@ import logging
 from threading import Thread
 from functools import partial
 
-logging.basicConfig(level=logging.ERROR,
-                    filename='logs.txt',
-                    datefmt='%d-%m-%Y %H:%M:%S',
-                    format='%(levelname)s  %(asctime)s   [%(message)s]')
+logging.basicConfig(
+    level=logging.ERROR,
+    filename="logs.txt",
+    datefmt="%d-%m-%Y %H:%M:%S",
+    format="%(levelname)s  %(asctime)s   [%(message)s]",
+)
 
-logger = logging.getLogger('statusChecker')
+logger = logging.getLogger("statusChecker")
 
-urls = ['https://www.google.com', 'https://www.youtube.com', 'https://www.facebook.com', 'https://www.twitter.com',
-        'https://www.python.org', 'https://www.stackoverflow.com', 'https://www.linkedin.com', 'https://www.quora.com',
-        'https://www.flipkart.com', 'https://www.medium.com']
+urls = [
+    "https://www.google.com",
+    "https://www.youtube.com",
+    "https://www.facebook.com",
+    "https://www.twitter.com",
+    "https://www.python.org",
+    "https://www.stackoverflow.com",
+    "https://www.linkedin.com",
+    "https://www.quora.com",
+    "https://www.flipkart.com",
+    "https://www.medium.com",
+]
 
 
 def make_request(url):
     try:
         response = requests.get(url, timeout=3)
         if response.status_code != 200:
-            return logger.error(f'{url} might be down')
-        print(f'OK for {url}')
+            return logger.error(f"{url} might be down")
+        print(f"OK for {url}")
     except:
-        logger.error(f'{url} might be down')
+        logger.error(f"{url} might be down")
 
 
 while True:
@@ -33,6 +44,6 @@ while True:
         thread.start()
     for thread in threads:
         thread.join()
-    print(f'Total time taken for the request pool = {time.time() - start}')
-    print('Going into hibernation for 5 seconds')
+    print(f"Total time taken for the request pool = {time.time() - start}")
+    print("Going into hibernation for 5 seconds")
     time.sleep(5)
